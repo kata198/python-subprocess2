8939d0506cf07a316312bc152dddddf6839798c6
Tim Savannah <kata198@gmail.com>
2016-09-07 14:20:30 -0400

    Tag 2.0.1 with case update on doc html

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='python-subprocess2',
-            version='2.0.0',
+            version='2.0.1',
             packages=['subprocess2'],
             author='Tim Savannah',
             author_email='kata198@gmail.com',
